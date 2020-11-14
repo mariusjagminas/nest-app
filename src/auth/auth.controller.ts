f116d61be26ea1de7b23d1fb10dd4fa2ab21a689
Marius Jagminas <marius.jagminas@rumblefish.pl>
2020-11-14 17:10:05 +0100

    Change entities array to autoLoad option

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,6 @@ export class AuthController {
 
   @Post('sign-up')
   signUp(@Body() dto: SignUpDto) {
-    this.authService.signUp(dto)
+   return this.authService.signUp(dto)
   }
 }
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,4 @@
 import { ConnectionOptions } from 'typeorm';
-import { User } from 'src/users/user.entity';
 
 export const getDbConfig = () => ({
   host: process.env.DB_HOST,
@@ -8,6 +7,6 @@ export const getDbConfig = () => ({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [User],
+  autoLoadEntities: true,
   synchronize: true
 }) as ConnectionOptions;
